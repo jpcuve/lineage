8037b6d5fd81f15abd698c3559c76fce0a1600f4
jpc <jpcuvelliez@gmail.com>
2018-10-21 22:20:29 +0200

    .

---
+++ src/main/java/com/messio/lineage/DataFacade.java
@@ -0,0 +1,39 @@
+package com.messio.lineage;
+
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.io.Serializable;
+import java.util.Objects;
+
+@Repository
+@Transactional
+public class DataFacade {
+    @PersistenceContext
+    private EntityManager em;
+
+    public <T> T create(T t){
+        Objects.requireNonNull(t);
+        em.persist(t);
+        em.flush();
+        em.refresh(t);
+        return t;
+    }
+
+    public <T, ID extends Serializable> T findOne(Class<T> clazz, ID pk){
+        Objects.requireNonNull(pk);
+        return em.find(clazz, pk);
+    }
+
+    public <T> T update(T t){
+        Objects.requireNonNull(t);
+        return em.merge(t);
+    }
+
+    public <T, ID extends Serializable> void delete(Class<T> clazz, ID pk){
+        Objects.requireNonNull(pk);
+        em.remove(em.getReference(clazz, pk));
+    }
+}
